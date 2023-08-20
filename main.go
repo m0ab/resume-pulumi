e1e1013405c94da2fbddcd952ac8ac67efc86523
Mark Bennett <marksg1mail@gmail.com>
2023-08-20 14:40:15 -0400

    fix(iam): create a read policy for making the bucket public accessible

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-    "io/ioutil"
+	"io/ioutil"
 )
 
 func main() {
@@ -21,23 +21,31 @@ func main() {
 			return err
 		}
 
-        
-        contentBytes, err := ioutil.ReadFile("index.html")
-        if err != nil {
-            return err
-        }
-        content := string(contentBytes)
-        
-        _, err = storage.NewBucketObject(ctx, "indexHtml", &storage.BucketObjectArgs{
-            Bucket:      bucket.Name,
-            Content:     pulumi.String(content),
-            ContentType: pulumi.String("text/html"),
-            Name:        pulumi.String("index.html"),
-        })
-        if err != nil {
-            return err
-        }
-        
+		// Set IAM policy for the bucket to be publicly readable
+		_, err = storage.NewBucketIAMMember(ctx, "publicReadPolicy", &storage.BucketIAMMemberArgs{
+			Bucket: bucket.Name,
+			Role:   pulumi.String("roles/storage.objectViewer"),
+			Member: pulumi.String("allUsers"),
+		})
+		if err != nil {
+			return err
+		}
+
+		contentBytes, err := ioutil.ReadFile("index.html")
+		if err != nil {
+			return err
+		}
+		content := string(contentBytes)
+
+		_, err = storage.NewBucketObject(ctx, "indexHtml", &storage.BucketObjectArgs{
+			Bucket:      bucket.Name,
+			Content:     pulumi.String(content),
+			ContentType: pulumi.String("text/html"),
+			Name:        pulumi.String("index.html"),
+		})
+		if err != nil {
+			return err
+		}
 
 		ctx.Export("bucketURL", pulumi.Sprintf("https://storage.googleapis.com/%s", bucket.Name))
 
