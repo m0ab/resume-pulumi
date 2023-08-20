c9ac6bef97f85fc39918963f0019c6fa227f984f
Mark Bennett <marksg1mail@gmail.com>
2023-08-20 16:02:27 -0400

    feat(web): add domain verification and stock 404

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		// Create a GCP Storage Bucket
 		bucket, err := storage.NewBucket(ctx, "myBucket", &storage.BucketArgs{
-			Name:     pulumi.String("markbennettresume"),
+			Name:     pulumi.String("www.markbennett.info"),
 			Location: pulumi.String("US"),
 			Website: &storage.BucketWebsiteArgs{
 				MainPageSuffix: pulumi.String("index.html"),
@@ -31,15 +31,14 @@ func main() {
 			return err
 		}
 
-		contentBytes, err := ioutil.ReadFile("index.html")
+		// Read and Upload index.html
+		indexContentBytes, err := ioutil.ReadFile("index.html")
 		if err != nil {
 			return err
 		}
-		content := string(contentBytes)
-
 		_, err = storage.NewBucketObject(ctx, "indexHtml", &storage.BucketObjectArgs{
 			Bucket:      bucket.Name,
-			Content:     pulumi.String(content),
+			Content:     pulumi.String(string(indexContentBytes)),
 			ContentType: pulumi.String("text/html"),
 			Name:        pulumi.String("index.html"),
 		})
@@ -47,7 +46,24 @@ func main() {
 			return err
 		}
 
+		// Read and Upload 404.html
+		notFoundContentBytes, err := ioutil.ReadFile("404.html")
+		if err != nil {
+			return err
+		}
+		_, err = storage.NewBucketObject(ctx, "error404Html", &storage.BucketObjectArgs{
+			Bucket:      bucket.Name,
+			Content:     pulumi.String(string(notFoundContentBytes)),
+			ContentType: pulumi.String("text/html"),
+			Name:        pulumi.String("404.html"),
+		})
+		if err != nil {
+			return err
+		}
+
 		ctx.Export("bucketURL", pulumi.Sprintf("https://storage.googleapis.com/%s", bucket.Name))
+
+
 
 		return nil
 	})
