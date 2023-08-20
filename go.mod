605a5ce766936ac05eb09f9e71a58b64db3085b2
Mark Bennett <marksg1mail@gmail.com>
2023-08-20 14:14:10 -0400

    feat(site): create minimum viable prototype of website with Pulumi

--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pkg/term v1.1.0 // indirect
 	github.com/pulumi/pulumi-gcp/sdk/v5 v5.26.0 // indirect
+	github.com/pulumi/pulumi-gcp/sdk/v6 v6.62.0 // indirect
 	github.com/pulumi/pulumi-google-native/sdk v0.31.1 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+    "io/ioutil"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create a GCP Storage Bucket
+		bucket, err := storage.NewBucket(ctx, "myBucket", &storage.BucketArgs{
+			Name:     pulumi.String("markbennettresume"),
+			Location: pulumi.String("US"),
+			Website: &storage.BucketWebsiteArgs{
+				MainPageSuffix: pulumi.String("index.html"),
+				NotFoundPage:   pulumi.String("404.html"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+        
+        contentBytes, err := ioutil.ReadFile("index.html")
+        if err != nil {
+            return err
+        }
+        content := string(contentBytes)
+        
+        _, err = storage.NewBucketObject(ctx, "indexHtml", &storage.BucketObjectArgs{
+            Bucket:      bucket.Name,
+            Content:     pulumi.String(content),
+            ContentType: pulumi.String("text/html"),
+            Name:        pulumi.String("index.html"),
+        })
+        if err != nil {
+            return err
+        }
+        
+
+		ctx.Export("bucketURL", pulumi.Sprintf("https://storage.googleapis.com/%s", bucket.Name))
+
+		return nil
+	})
+}
